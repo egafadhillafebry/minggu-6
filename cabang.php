57a6fad60f36934d9c51146b9f1386eb5331e419
Ega Fadhilla Febryana <100186624+egafadhillafebry@users.noreply.github.com>
2022-03-29 22:28:16 +0700

    Add files via upload

---
+++ cabang.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $nama = "Ega Fadhilla Febryana";
+
+            if($nama=="Ega Fadhilla Febryana"){
+                echo "Selamat belajar $nama";
+            }
+        ?>
+    </body>
+</html>
---
+++ ulang2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $colors = array("red", "green", "blue", "yelow");
+
+            foreach ($colors as $value) {
+            echo "$value <br>";
+            }        
+        ?>
+    </body>
+</html>
